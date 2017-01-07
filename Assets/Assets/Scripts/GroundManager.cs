32652677f780ce00af43adb502c35a849826080a
Dagur Arinbjörn Daníelsson <dagurdan@simnet.is>
2017-01-07 12:20:08 +0000

    scrolling ground

---
+++ Assets/Assets/Scripts/GroundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour {
+
+    public List<GameObject> sprites;
+
+    public BoxCollider2D outBox;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // FixedUpdate is called once per physics frame
+    private void FixedUpdate() {
+        // move all objects allong the x axis
+        foreach (GameObject obj in sprites){
+            obj.transform.Translate(new Vector2(-0.1f, 0));
+        }
+
+        // if the leftmost object left the frame move it to the end
+        GameObject o = sprites[0];
+        if (isOutOfFrame(o)){
+            o.GetComponent<Rigidbody2D>().MovePosition(new Vector2(sprites[sprites.Count - 1].transform.position.x + (((RectTransform)o.transform).rect.width*5), 
+                                                                   o.transform.position.y));
+
+            sprites.RemoveAt(0);
+            sprites.Add(o);
+        }
+    }
+
+    /**
+     * Check if object has left frame on the left
+     */
+    private bool isOutOfFrame(GameObject obj){
+        return outBox.IsTouching(obj.GetComponent<BoxCollider2D>());
+    }
+}
