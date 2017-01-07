8f9a80e0e269193cee439c01e8949561aa6105a7
Dagur Arinbjörn Daníelsson <dagurdan@simnet.is>
2017-01-07 18:29:56 +0000

    main menu ver 1

---
+++ Assets/Assets/GuiAssets/Scripts/UIManagerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+    [SerializeField]
+    private Animator m_startButton;
+    [SerializeField]
+    private Animator m_howToButton;
+    [SerializeField]
+    private Animator m_settingsButton;
+    [SerializeField]
+    private Animator m_settingsDialog;
+    [SerializeField]
+    private Animator m_howToDialog;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame() {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OpenSettings() {
+        SetAllButtonsVisible(false);
+        m_settingsDialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings() {
+        m_settingsDialog.SetBool("isHidden", true);
+        SetAllButtonsVisible(true);
+    }
+
+    public void OpenHowTo() {
+        SetAllButtonsVisible(false);
+        m_howToDialog.SetBool("isHidden", false);
+    }
+
+    public void CloseHowTo() {
+        m_howToDialog.SetBool("isHidden", true);
+        SetAllButtonsVisible(true);
+    }
+
+    private void SetAllButtonsVisible(bool visible) {
+        visible = !visible;
+        m_startButton.SetBool("isHidden", visible);
+        m_howToButton.SetBool("isHidden", visible);
+        m_settingsButton.SetBool("isHidden", visible);
+    }
+}
