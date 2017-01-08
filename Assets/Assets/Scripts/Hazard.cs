4df098630996f02050f19c53434f712c42e4bb23
Dagur Arinbjörn Daníelsson <dagurdan@simnet.is>
2017-01-08 12:11:04 +0000

    just to be safe commit

--- Assets/Assets/Scripts/Hazard.cs
+++ Assets/Assets/Scripts/Hazard.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Hazard : MonoBehaviour {
 
     [SerializeField]
-    private float m_speed = 0.11f;
+    private float m_speed = 0.2f;
     [SerializeField]
     private bool m_isCrate;
 
